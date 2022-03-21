cd66aaee2dc7e0ec260f4d98f08edbda99aba29a
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-21 17:29:08 +0000

    added buttons

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* Created by: Kenny Le
+ * Created on: Mar 2022
+ * This file contains the JS functions for index.html 
+*/
+
+/**
+ * This function gets users street number and name and shows it back to user.
+ */
+function myButtonClicked() {
+  // input
+  const street-number = parseInt(document.getElementById("street-number").value)
+  const street-name = document.getElementById("street-name").value
+  
+  // output
+  document.getElementById
+  
+}
